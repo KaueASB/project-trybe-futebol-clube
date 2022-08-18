d747d293cde57efd81558743e389fb3f88288426
Kaue Alves <kaue_alves00@yahoo.com>
2022-08-18 14:08:27 -0300

    feat/ concluindo o requisito 4 e 5

--- app/backend/src/services/validations.ts
+++ app/backend/src/services/validations.ts
@@ -1,16 +1,23 @@
-// import Joi = require('joi');
-// import { ILogin } from '../interfaces/Interfaces';
+import Joi = require('joi');
+import ThrowErrors from '../middlewares/ThrowErros';
+import { ILogin } from '../interfaces/Interfaces';
 
-// const validation = {
-//   async login(body: ILogin) {
-//     const schema = Joi.object({
-//       email: Joi.string().required().email(),
-//       password: Joi.string().min(7).required(),
-//     });
+const validation = {
+  async login(body: ILogin) {
+    try {
+      const schema = Joi.object({
+        email: Joi.string().required().email().messages({
+          'string.email': 'All fields must be filled',
+        }),
+        password: Joi.string().min(7).required(),
+      });
 
-//     const result = await schema.validateAsync(body);
-//     return result;
-//   },
-// };
+      const result = await schema.validateAsync(body);
+      return result;
+    } catch (error) {
+      throw new ThrowErrors('validationError', 'All fields must be filled');
+    }
+  },
+};
 
-// export default validation;
+export default validation;
